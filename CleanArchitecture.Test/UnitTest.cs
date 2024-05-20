abb736a29e2a0b72cbc64b39704c717bdebffd08
Lívia Coelho <livia.muniz1@aluno.ce.gov.br>
2024-05-20 14:17:53 -0300

    Implementação das camadas de dominio, aplicação, infraestrutura e de apresentação

---
+++ CleanArchitecture.Test/UnitTest.cs
@@ -0,0 +1,11 @@
+namespace CleanArchitecture.Test
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ WebApplication1/Program.cs
@@ -0,0 +1,35 @@
+using CleanArchitecture.API.Extensions;
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigurePersistenceApp(builder.Configuration);
+builder.Services.ConfigureApplicationApp();
+builder.Services.ConfigureCorsPolicy();
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors();
+app.MapControllers();
+
+app.Run();
